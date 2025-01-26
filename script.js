92c5218b59f1472fb8ab8e075dc6968e07af960a
Branham Karanja <branham.karanja@outlook.com>
2025-01-26 22:08:39 +0300

    completed second project

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+// This is a callback function that will be called when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
-	// This is a callback function that will be called when the DOM is fully loaded
+	const loadTasks = () => {
+		const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+		storedTasks.forEach((taskText) => {
+			addTask(taskText, false);
+			console.log("taskText");
+		});
+	};
+
 	// Selection of DOM elements
 	const addButton = document.getElementById("add-task-btn");
 	const taskInput = document.getElementById("task-input");
 	const taskList = document.getElementById("task-list");
 
 	// Function to add a task
-	const addTask = () => {
-		const taskText = taskInput.value.trim();
+	const addTask = (task, save = true) => {
+		// const taskText = taskInput.value.trim();
+		const taskText = task || taskInput.value.trim();
 
 		if (!taskText) {
 			alert("Please enter a task");
@@ -20,19 +29,36 @@ document.addEventListener("DOMContentLoaded", () => {
 			deleteButton.textContent = "Remove";
 			deleteButton.onclick = () => {
 				item.remove();
+                const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+                const taskIndex = storedTasks.indexOf(taskText);
+                storedTasks.splice(taskIndex, 1);
+                localStorage.setItem("tasks", JSON.stringify(storedTasks));
 			};
 			item.appendChild(deleteButton);
 			taskList.appendChild(item);
 			taskInput.value = "";
+
+			// Save tasks to local storage
+			if (save) {
+				const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+				storedTasks.push(taskText);
+				localStorage.setItem("tasks", JSON.stringify(storedTasks));
+			}
 		}
 	};
+	loadTasks();
 
 	// Event listeners for the add button and the enter key
-	addButton.addEventListener("click", addTask);
+	addButton.addEventListener("click", () => {
+		addTask();
+	});
 	taskInput.addEventListener("keypress", (event) => {
 		if (event.key === "Enter") {
 			addTask();
 		}
 	});
-	document.addEventListener("DOMContentLoaded", addTask);
+	document.addEventListener("DOMContentLoaded", () => {
+		addTask();
+	});
 });
+// localStorage.clear();
